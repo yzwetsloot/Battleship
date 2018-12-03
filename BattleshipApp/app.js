6956246314f493f6f6d8b5f098a08f4c8922c637
Max van Dijk <maxvandijk2000@gmail.com>
2018-12-03 14:53:00 +0100

    Added routes

--- BattleshipApp/app.js
+++ BattleshipApp/app.js
@@ -1,13 +1,11 @@
 var express = require("express");
 var http = require("http");
-
+var router = express.Router();
 
 
 var port = process.argv[2];
 
 var app = express();
-
-
 
 app.use(express.static(__dirname + "/public"));
 http.createServer(app).listen(port);
@@ -16,4 +14,8 @@ app.get('/', function(req, res, next) {
   res.sendFile("splash.html", {root: "./public"});
 });
 
+
+app.get("/play", function(req, res,next) {
+  res.sendFile("game.html", {root: "./public"});
+});
 

7f2c25c87cd3e17c646b78dcbe75a37a4fd4d427
yzwetsloot <yourizwetsloot@yahoo.com>
2018-12-03 14:57:29 +0100

    Useless changes

--- BattleshipApp/routes/index.js
+++ BattleshipApp/routes/index.js
@@ -1,13 +1,14 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/splash", function (req, res) {
+    res.sendFile("splash.html", {root: "./public"});
 });
 
-router.get("/play", function(req, res){
-  res.sendFile("game.html", {root: "./plubic"})
-})
+/* Pressing the 'PLAY' button, returns this page */
+router.get("/play", function(req, res) {
+    res.sendFile("game.html", {root: "./public"});
+});
 
 module.exports = router;

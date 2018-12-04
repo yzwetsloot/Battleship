21c062794d7a87a036f9753454c96322e8075d7d
yzwetsloot <yourizwetsloot@yahoo.com>
2018-12-04 14:05:47 +0100

    Add comments to app.js

--- BattleshipApp/app.js
+++ BattleshipApp/app.js
@@ -1,17 +1,23 @@
+//require express- and http-modules
 var express = require("express");
 var http = require("http");
 
+//allow third argument in terminal to be port 
 var port = process.argv[2];
 
+//let app be express
 var app = express();
 
+//make app into a static file server, make it listen on port (third argument in terminal)
 app.use(express.static(__dirname + "/public"));
 http.createServer(app).listen(port);
 
+//setup root route, response splash.html from root /public
 app.get('/', function(req, res, next) {
   res.sendFile("splash.html", {root: "./public"});
 });
 
+//setup route /play which loads game.html
 app.get("/play", function(req, res,next) {
   res.sendFile("game.html", {root: "./public"});
 });

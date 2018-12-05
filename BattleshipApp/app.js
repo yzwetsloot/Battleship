7cb016a0ea05e02403d75a58707cf9989bfb32d9
yzwetsloot <yourizwetsloot@yahoo.com>
2018-12-05 22:49:30 +0100

    Adjust server logs

--- BattleshipApp/app.js
+++ BattleshipApp/app.js
@@ -2,8 +2,9 @@
 var express = require("express");
 var http = require("http");
 var websocket = require("ws")
+var net = require("net");
 
-//allow third argument in terminal to be port 
+//allow third argument in terminal to be port, usually 3000
 var port = process.argv[2];
 
 //let app be express
@@ -11,7 +12,17 @@ var app = express();
 
 //make app into a static file server, make it listen on port (third argument in terminal)
 app.use(express.static(__dirname + "/public"));
-http.createServer(app).listen(port);
+
+//create server, let it listen on port provided as third argument in terminal window
+//callback function prints to console that server is started and it's listening for subscribers
+http.createServer(app).listen(port, function () {
+  console.log("Server started\nListening for subscribers...")
+});
+
+/*var server = net.createServer(function (connection) {
+
+  //what to do on connect
+});*/
 
 //setup root route, response splash.html from root /public
 app.get('/', function(req, res, next) {
